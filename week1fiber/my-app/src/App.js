5fffdc325fd45a09bf8f17df96586e9b058ccb7a
Joyce Zheng <yz4867@nyu.edu>
2024-09-12 20:24:20 -0400

    light control

--- week1fiber/my-app/src/App.js
+++ week1fiber/my-app/src/App.js
@@ -1,43 +1,65 @@
 import logo from './logo.svg';
+
 import './App.css';
 import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Scene from './Cat';
 import { AmbientLight } from 'three';
 import { OrbitControls } from '@react-three/drei'
+import StaticUI from './StaticUI';
+import { useStore } from './store';
+import { useEffect } from 'react';
+import Ground from './Ground';
+import Flower from './Flower';
 
 function App() {
+
+
+  const modelActive = useStore(state => state.modelActive);
+  const flowerActive = useStore(state => state.flowerActive);
+
+  useEffect(() => {
+
+    if (modelActive === true) {
+      console.info("modelActive=true");
+    }
+  }, [modelActive]);
+
+  useEffect(() => {
+
+    if (flowerActive === true) {
+      console.info("flowerActive=true");
+    }
+  }, [flowerActive]);
+
+  // if (modelActive == true) {
+  //   console.info("modelactive=true")
+  // }
   return (
     <div className="App">
+
       <div id="canvas-container">
+
+        <StaticUI />
+
         <Canvas>
-          <ambientLight intensity={8.0} />
+          <ambientLight intensity={1.0} />
+          {flowerActive && <pointLight position={[-1, 0, 1]} intensity={30} />}
+
           <mesh>
 
-            <boxGeometry />
-            <Scene />
+            {/* <boxGeometry /> */}
+            {modelActive && <Scene />}
+            {/* <Flower /> */}
+
+            <Ground />
 
             <meshStandardMaterial />
           </mesh>
           <OrbitControls />
         </Canvas>
 
-
       </div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
---
+++ week1fiber/my-app/src/Ground.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+
+import './App.css';
+
+
+import { DoubleSide } from "three";
+
+
+export default function Ground() {
+
+
+    return (
+
+
+        <mesh position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[3, 3, 3]}>
+            {/* <boxGeometry /> */}
+            <planeGeometry />
+            <meshStandardMaterial color="green" side={DoubleSide} />
+        </mesh>
+
+
+
+
+    );
+}
+
+
